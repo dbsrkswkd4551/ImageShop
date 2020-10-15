025b82c6db30f17b1a56e8a9627a9c8be703100c
dbsrkswkd4551 <ysang4512005@naver.com>
2020-10-15 16:15:23 +0900

    댓글추가d

--- src/main/java/org/hdcd/mapper/CommentMapper.java
+++ src/main/java/org/hdcd/mapper/CommentMapper.java
@@ -2,9 +2,11 @@ package org.hdcd.mapper;
 
 import org.hdcd.common.domain.PageRequest;
 import org.hdcd.domain.Comment;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository("org.hdcd.mapper.CommentMapper")
 public interface CommentMapper {
 
 
